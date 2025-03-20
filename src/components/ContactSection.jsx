cc163869c77cad948c6bb8f522d84aa9a58df216
Soumyadip Roy <soumyaroy172003@gmail.com>
2025-03-20 10:28:12 +0530

    Improved UI Contact Us page

--- src/components/ContactSection.jsx
+++ src/components/ContactSection.jsx
@@ -1,19 +1,34 @@
 import { CONTACT } from "../constants";
+import { motion } from "framer-motion";
 const ContactSection = () => {
   return (
     <>
       <section id="contact" className="container mx-auto py-16">
-        <h2 className="mb-8  text-center text-3xl lg:text-4xl">Contact Us</h2>
-        <div className="text-neutral-400">
+        <motion.h2
+          initial={{ opacity: 0, y: 50 }}
+          whileInView={{ opacity: 1, y: 0 }}
+          viewport={{ once: true }}
+          transition={{ duration: 0.6, delay: 0.2 }}
+          className="text-4xl tracking-tighter lg:text-6xl text-center"
+          // className="mb-8  text-center text-3xl lg:text-4xl"
+        >
+          Contact Us
+        </motion.h2>
+        <motion.div className="text-neutral-400">
           {CONTACT.map((detail) => (
-            <p
+            <motion.p
               key={detail.key}
-              className="my-20 border-b-2 border-dotted border-neutral-700 text-center text-2xl tracking-tighter lg:text-3xl"
+              // className=" "
+              initial={{ opacity: 0, y: 50 }}
+              whileInView={{ opacity: 1, y: 0 }}
+              viewport={{ once: true }}
+              transition={{ duration: 0.6, delay: 0.6 }}
+              className="my-20 border-b-2 border-dotted border-neutral-700 text-center text-2xl tracking-tighter lg:text-3xl "
             >
               {detail.value}
-            </p>
+            </motion.p>
           ))}
-        </div>
+        </motion.div>
       </section>
     </>
   );
--- src/pages/Contact_Us.jsx
+++ src/pages/Contact_Us.jsx
@@ -19,60 +19,71 @@ const Contact_Us = () => {
 
   return (
     <>
-      <ContactSection />
       <section className="container mt-20 mx-auto" id="our_location">
-        <h2 className="text-center text-4xl lg:text-4xl">Our Location</h2>
-        <div className="py-10 flex flex-wrap">
-          <div className="w-full z-10 p-6 lg:w-1/2 rounded-3xl ">
-            <MapContainer
-              center={[12.856022368965954, 77.46562519109459]} // Adjust the coordinates to your desired location
-              zoom={13}
-              scrollWheelZoom={true}
-              style={{ height: "500px", width: "100%" }} // Adjust the size as needed
-            >
-              <TileLayer
-                url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-                attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-              />
-              <Marker
-                position={[12.856022368965954, 77.46562519109459]}
-                icon={customIcon}
+        <ContactSection />
+        <section className="container mt-20 mx-auto" id="our_location">
+          <motion.h2
+            initial={{ opacity: 0, y: 50 }}
+            whileInView={{ opacity: 1, y: 0 }}
+            viewport={{ once: true }}
+            transition={{ duration: 0.6, delay: 0.2 }}
+            className="text-4xl text-center tracking-tighter lg:text-6xl"
+            // className="text-center text-4xl lg:text-4xl"
+          >
+            Our Location
+          </motion.h2>
+          <div className="py-10 flex flex-wrap">
+            <div className="w-full z-10 p-6 lg:w-1/2 rounded-3xl ">
+              <MapContainer
+                center={[12.856022368965954, 77.46562519109459]} // Adjust the coordinates to your desired location
+                zoom={13}
+                scrollWheelZoom={true}
+                style={{ height: "500px", width: "100%" }} // Adjust the size as needed
               >
-                <Popup>
-                  We are here! <br /> PRAJAKIRANA SEVA CHARITABLE TRUST(R).
-                </Popup>
-              </Marker>
-            </MapContainer>
+                <TileLayer
+                  url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+                  attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+                />
+                <Marker
+                  position={[12.856022368965954, 77.46562519109459]}
+                  icon={customIcon}
+                >
+                  <Popup>
+                    We are here! <br /> PRAJAKIRANA SEVA CHARITABLE TRUST(R).
+                  </Popup>
+                </Marker>
+              </MapContainer>
+            </div>
+            <div className="w-full px-2 lg:w-1/2">
+              <motion.h2
+                initial={{ opacity: 0, y: 50 }}
+                whileInView={{ opacity: 1, y: 0 }}
+                viewport={{ once: true }}
+                transition={{ duration: 0.6, delay: 0.2 }}
+                className="text-4xl tracking-tighter lg:text-6xl"
+              >
+                We are Here !
+              </motion.h2>
+              <motion.div
+                initial={{ opacity: 0, y: 50 }}
+                whileInView={{ opacity: 1, y: 0 }}
+                viewport={{ once: true }}
+                transition={{ duration: 0.6, delay: 0.4 }}
+                className="mb-8 mt-1 h-2 w-36 bg-green-300 lg:rotate-3"
+              ></motion.div>
+              <motion.p
+                initial={{ opacity: 0, y: 50 }}
+                whileInView={{ opacity: 1, y: 0 }}
+                viewport={{ once: true }}
+                transition={{ duration: 0.6, delay: 0.6 }}
+                className="m-8 text-2xl leading-relaxed tracking-tight lg:max-w-xl"
+              >
+                VF48+878, 26/2, Devagere, Kaggalipur Road, Kumbalgodu,
+                Bengaluru, Karnataka 560060
+              </motion.p>
+            </div>
           </div>
-          <div className="w-full px-2 lg:w-1/2">
-            <motion.h2
-              initial={{ opacity: 0, y: 50 }}
-              whileInView={{ opacity: 1, y: 0 }}
-              viewport={{ once: true }}
-              transition={{ duration: 0.6, delay: 0.2 }}
-              className="text-4xl tracking-tighter lg:text-6xl"
-            >
-              We are Here !
-            </motion.h2>
-            <motion.div
-              initial={{ opacity: 0, y: 50 }}
-              whileInView={{ opacity: 1, y: 0 }}
-              viewport={{ once: true }}
-              transition={{ duration: 0.6, delay: 0.4 }}
-              className="mb-8 mt-1 h-2 w-36 bg-green-300 lg:rotate-3"
-            ></motion.div>
-            <motion.p
-              initial={{ opacity: 0, y: 50 }}
-              whileInView={{ opacity: 1, y: 0 }}
-              viewport={{ once: true }}
-              transition={{ duration: 0.6, delay: 0.6 }}
-              className="m-8 text-2xl leading-relaxed tracking-tight lg:max-w-xl"
-            >
-              VF48+878, 26/2, Devagere, Kaggalipur Road, Kumbalgodu, Bengaluru,
-              Karnataka 560060
-            </motion.p>
-          </div>
-        </div>
+        </section>
       </section>
     </>
   );
